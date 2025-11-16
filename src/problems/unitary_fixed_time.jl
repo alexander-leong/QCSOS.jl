ae4332cd45e6c2caf41bcda8eb6cd3ffb56f485b
Alexander Leong <toshiba_alexander@live.com>
2025-11-16 20:07:58 +1000

    WIP

--- src/problems/unitary_fixed_time.jl
+++ src/problems/unitary_fixed_time.jl
@@ -1,3 +1,4 @@
+using CUDA
 using ConicSolve
 
 include("../popt.jl")
@@ -16,30 +17,39 @@ function set_equality_constraints(A, As, b, bs, i, num_blocks, num_vars, row_ind
     #   0 ... 0  As[2]  0 ... 0
     #   0 ... 0 0 ... 0  As[3]
     A[row_start_idx:row_end_idx, col_start_idx:col_end_idx] = As[i]
-    append!(b, bs[i])
+    b[row_start_idx:row_end_idx] = bs[i]
 
     # set poly - t + delta = 0 s.t. (t, delta) >= 0 and
     # where "t" is the variable (scalar) to optimize
     # There are two additional variables, "t" and "delta":
     #   - x[end-1] is "t"
     #   - x[end] is "delta"
-    A[end, col_start_idx:col_end_idx] .= 1
-    A[end, end-num_blocks-i] = -1
-    A[end, end-i] = 1
-    b = push!(b, 0)
+    A[end-i+1, col_start_idx:col_end_idx] .= 1
+    # set t_i
+    A[end-i+1, end-num_blocks-i+1] = -1
+    # set delta_i
+    A[end-i+1, end-i+1] = 1
+    b[end-i+1] = 0
 
     # constraints on the control coefficients
     # elements in tr_gram_A share the same control coefficients
     num_coeffs = size(As[1], 2)
     if i >= 2
         for j in 1:num_coeffs
-            A_i = zeros(num_coeffs)
+            A_i = zeros((1, size(A, 2)))
             A_i[j] = 1
-            A_i[num_coeffs * i + j] = -1
+            A_i[num_coeffs * (i-1) + j] = -1
             A = vcat(A, A_i)
-            b = push!(b, 0)
+            b = append!(b, 0)
         end
     end
+    return A, b
+end
+
+function set_objective(num_blocks, total_num_cols)
+    c = zeros(total_num_cols)
+    c[end-num_blocks+1:end] .= 1
+    return c
 end
 
 function QuantumUnitaryFixedTimeProblem(A, x)
@@ -59,6 +69,7 @@ function QuantumUnitaryFixedTimeProblem(A, x)
     psds = nothing
     num_additional_vars = 2
     total_num_additional_vars = num_additional_vars * num_blocks
+    
     # num_vars = Int(sum([size(x, 1) * (size(x, 1) + 1)/2 for x in psds]))
     num_vars = 0
     Threads.@threads for i in 1:num_blocks
@@ -71,54 +82,56 @@ function QuantumUnitaryFixedTimeProblem(A, x)
         As[i] = A_i
         bs[i] = b_i
     end
-    return As, bs, psds
+
     num_rows = [size(A, 1) for A in As]
-    total_num_rows = sum(num_rows)
+    total_num_rows = sum(num_rows) + num_blocks
     total_num_cols = num_blocks * num_vars + total_num_additional_vars
     A = zeros((total_num_rows, total_num_cols))
-    row_inds = [0, cumsum(num_rows)...]
+	b = zeros(total_num_rows)
+
     # block constraints
+    row_inds = [0, cumsum(num_rows)...]
     for i in 1:num_blocks
-        set_equality_constraints(A, As, b, bs, i, num_blocks, num_vars, row_inds)
+        A, b = set_equality_constraints(A, As, b, bs, i, num_blocks, num_vars, row_inds)
     end
-    return As, bs, psds
     
-    # FIXME!
-    # The vector "c_i" is [0 ... 0 -1]
-    c_i = zeros(num_vars + 2)
-    c_i = set_objective(c_i)
-    c = push!(c, c_i)
+    # The vector "c" is [0 ... 0 -1 ... -1]
+    c = set_objective(num_blocks, total_num_cols)
     
-    for psds_i in psds
-        p = size(psds_i, 1)
-        push!(cones, PSDCone(p))
+    for _ in 1:num_blocks
+        for psds_i in psds
+            p = size(psds_i, 1)
+            push!(cones, PSDCone(p))
+        end
+        push!(cones, NonNegativeOrthant(num_additional_vars))
     end
-    push!(cones, NonNegativeOrthant(2))
 
     num_vars = size(A, 2)
     G = Matrix{Float64}(I, num_vars, num_vars)
-    h = zeros(num_vars)
     P = zeros((num_vars, num_vars))
+    h = zeros(num_vars)
 
     # Get problem
     cone_qp = ConeQP{Float64, Float64, Float64}(A, G, P, b, c, h, cones)
-    return cone_qp, num_coeffs, psds
+    return cone_qp, psds
 end
 
 # Solve problem
 function QuantumUnitaryFixedTimeProblem_QSOS(A, x)
-    cone_qp, num_coeffs, psds = QuantumUnitaryFixedTimeProblem(A, x)
+    cone_qp, psds = QuantumUnitaryFixedTimeProblem(A, x)
     kktsolve = "qrchol"
     solver = Solver(cone_qp, kktsolve)
-    solver.max_iterations = 10
+    solver.device = GPU
+    solver.max_iterations = 1
     status = run_solver(solver)
 
-    min_x = get_solution(solver)
-    min_x = x[1:num_coeffs]
+    # min_x = get_solution(solver)
+    # min_x = x[1:num_coeffs]
 
-    min_x = get_value_in_original_basis(psds, min_x)
+    # min_x = get_value_in_original_basis(psds, min_x)
 
-    return min_x
+    # return min_x
+    return
 end
 
 export QuantumUnitaryFixedTimeProblem
